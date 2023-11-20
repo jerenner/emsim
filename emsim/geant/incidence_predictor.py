f809f9005ba627121eef1ba730b1982716a7c883
Matthew A. Wright <mawright@lbl.gov>
2023-11-20 13:48:03 -0800

    update gaussian incidence point predictor: add clamp_min to covariance matrix

--- emsim/geant/incidence_predictor.py
+++ emsim/geant/incidence_predictor.py
@@ -5,19 +5,12 @@ from torch.distributions import MultivariateNormal
 
 
 class GaussianFullRankIncidencePointPredictor(nn.Module):
-    def __init__(
-        self,
-        backbone,
-        hidden_dim=512,
-        mean_parameterization="sigmoid",
-        diagonal_covariance=False,
-        eps=1e-6,
-    ):
+    def __init__(self, backbone, hidden_dim=512, mean_parameterization="sigmoid", diagonal_covariance=False, eps=1e-6):
         super().__init__()
         self.mean_parameterization = mean_parameterization
         self.backbone = backbone
         self.diagonal_covariance = diagonal_covariance
-        self.eps = 1e-6
+        self.eps = eps
 
         if diagonal_covariance:
             out_dim = 4
@@ -34,18 +27,17 @@ class GaussianFullRankIncidencePointPredictor(nn.Module):
         if x.ndim == 3:
             x = x.unsqueeze(1)
         patch_shape = torch.as_tensor(x.shape[-2:], dtype=torch.float, device=x.device)
-        patch_center_coords = patch_shape / 2
+        # patch_center_coords = patch_shape / 2
         x = self.backbone(x)
         x = self.predictor(x)
 
         if self.diagonal_covariance:
             mean_vector, cholesky_diagonal = torch.split(x, [2, 2], -1)
         else:
-            mean_vector, cholesky_diagonal, cholesky_offdiag = torch.split(
-                x, [2, 2, 1], -1
-            )
+            mean_vector, cholesky_diagonal, cholesky_offdiag = torch.split(x, [2, 2, 1], -1)
 
         cholesky_diagonal = cholesky_diagonal.exp()
+        cholesky_diagonal = torch.clamp_min(cholesky_diagonal, self.eps)
         cholesky = torch.diag_embed(cholesky_diagonal)
         if not self.diagonal_covariance:
             tril_indices = torch.tril_indices(2, 2, offset=-1, device=x.device)
@@ -59,13 +51,5 @@ class GaussianFullRankIncidencePointPredictor(nn.Module):
             inverse_scaling_matrix = torch.diag_embed(1 / patch_shape)
             mean_vector = torch.squeeze(scaling_matrix @ mean_vector.unsqueeze(-1), -1)
             cholesky = inverse_scaling_matrix @ cholesky
-
-        cholesky = (
-            cholesky
-            + torch.eye(
-                cholesky.shape[-1], dtype=cholesky.dtype, device=cholesky.device
-            )
-            * self.eps
-        )
 
         return MultivariateNormal(mean_vector, scale_tril=cholesky)

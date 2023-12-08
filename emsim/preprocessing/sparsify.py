bfe02155b235566c609b375d94dce88af966c162
Matthew A. Wright <mawright@lbl.gov>
2023-12-08 14:02:42 -0800

    update sparsify.py

--- emsim/preprocessing/sparsify.py
+++ emsim/preprocessing/sparsify.py
@@ -5,9 +5,24 @@ import numpy as np
 from scipy.ndimage import binary_dilation
 
 
+class NSigmaSparsifyTransform:
+    def __init__(self, background_threshold_n_sigma=4, window_size=7):
+        self.background_threshold_n_sigma = background_threshold_n_sigma
+        self.window_size = window_size
+
+    def __call__(self, x):
+        return torch_sigma_energy_threshold_sparsify(
+            x,
+            background_threshold_n_sigma=self.background_threshold_n_sigma,
+            window_size=self.window_size,
+        )
+
+
 def torch_sigma_energy_threshold_sparsify(
     image: torch.Tensor, background_threshold_n_sigma=4, window_size=7
 ):
+    if window_size % 2 != 1:
+        raise ValueError(f"Expected an odd `window_size`, got {window_size=}")
     if image.ndim > 3:
         reduce_dims = [-1, -2, -3]
     else:

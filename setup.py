7c7957e04c39336a244ed427bf3f089db4e6df86
Matthew A. Wright <mawright@lbl.gov>
2023-12-14 11:42:56 -0800

    add 'sparse' to dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='emsim',
-    version='0.1',
-    description='electron detection image processing with deep learning',
-    packages=['emsim'],
+    name="emsim",
+    version="0.1",
+    description="electron detection image processing with deep learning",
+    packages=["emsim"],
     install_requires=[
-        'numpy',
-        'pandas',
-        'matplotlib',
-        'torch',
-        'scipy',
-        'torchvision',
-        'pillow',
-        'h5py',
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "torch",
+        "scipy",
+        "torchvision",
+        "pillow",
+        "h5py",
+        "sparse",
     ],
 )

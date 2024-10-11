bc8aa5afb9b94abc55a197edda880d9e86a6d1a9
Matthew Wright <mawright@lbl.gov>
2024-10-11 09:58:20 -0700

    code clarification for segmentation map predictor

--- emsim/networks/segmentation_map.py
+++ emsim/networks/segmentation_map.py
@@ -27,20 +27,21 @@ class SegmentationMapPredictor(nn.Module):
         batched_queries, query_pad_mask = deconcat_add_batch_dim(
             queries, query_batch_offsets
         )
-        feature_map = stacked_feature_map.unbind(-2)[-1].coalesce()
-        assert feature_map.ndim == 4  # (batch, height, width, feature)
+        # unbind over the level dimension
+        fullscale_feature_map = stacked_feature_map.unbind(-2)[-1].coalesce()
+        assert fullscale_feature_map.ndim == 4  # (batch, height, width, feature)
         feature_map_batch_offsets = batch_offsets_from_sparse_tensor_indices(
-            feature_map.indices()
+            fullscale_feature_map.indices()
         )
         batched_features, feature_pad_mask = deconcat_add_batch_dim(
-            feature_map.values(), feature_map_batch_offsets
+            fullscale_feature_map.values(), feature_map_batch_offsets
         )
         batched_feature_indices, index_pad_mask = deconcat_add_batch_dim(
-            feature_map.indices().T, feature_map_batch_offsets
+            fullscale_feature_map.indices().T, feature_map_batch_offsets
         )
         assert torch.equal(feature_pad_mask, index_pad_mask)
 
-        segmentation_logits = torch.bmm(batched_features, batched_queries.mT)
+        segmentation_logits = torch.bmm(batched_features, batched_queries.mT) # (batch, pixel/token, query)
 
         # now put the segmentation logits in a sparse tensor
         query_index = torch.arange(
@@ -54,17 +55,17 @@ class SegmentationMapPredictor(nn.Module):
                 query_index.expand(*segmentation_logits.shape[:-1], -1).unsqueeze(-1),
             ],
             -1,
-        )
+        ) # (batch, height, width, query)
         segmentation_logit_pad_mask = torch.logical_or(
             query_pad_mask.unsqueeze(-2), feature_pad_mask.unsqueeze(-1)
         )
-        nonpad_per_batch = segmentation_logit_pad_mask.logical_not().sum([-1, -2])
-        out_logits = segmentation_logits.new_zeros(nonpad_per_batch.sum().item())
+        num_nonpad_per_image = segmentation_logit_pad_mask.logical_not().sum([-1, -2])
+        out_logits = segmentation_logits.new_zeros(num_nonpad_per_image.sum().item())
         out_indices = segmentation_logit_indices.new_zeros(
-            nonpad_per_batch.sum().item(), 4
+            num_nonpad_per_image.sum().item(), 4
         )
         out_offsets = torch.cat(
-            [nonpad_per_batch.new_zeros([1]), nonpad_per_batch.cumsum(-1)]
+            [num_nonpad_per_image.new_zeros([1]), num_nonpad_per_image.cumsum(-1)]
         ).to("cpu")
 
         for batch_pad_mask, batch_logits, batch_indices, start_index, stop_index in zip(
@@ -84,8 +85,8 @@ class SegmentationMapPredictor(nn.Module):
         return torch.sparse_coo_tensor(
             out_indices.T,
             out_logits,
-            (*feature_map.shape[:-1], segmentation_logits.shape[-1]),
-            device=feature_map.device,
+            (*fullscale_feature_map.shape[:-1], segmentation_logits.shape[-1]),
+            device=fullscale_feature_map.device,
         ).coalesce()
 
 

fb13d3bc4b00513746919a9b287315f5baf93c9b
Matthew Wright <mawright@lbl.gov>
2024-12-12 17:15:29 -0800

    fix dice loss optimization

--- emsim/networks/loss/criterion.py
+++ emsim/networks/loss/criterion.py
@@ -448,14 +448,20 @@ class EMCriterion(nn.Module):
     ) -> Tensor:
         assert sorted_predicted_logits.shape == sorted_true.shape
 
-        predicted_segmentation = torch.sparse.softmax(sorted_predicted_logits, -1)
+        predicted_segmentation: Tensor = torch.sparse.softmax(sorted_predicted_logits, -1)
+        # predicted_segmentation.register_hook(lambda x: print("predicted_segmentation"))
+
+        # unioned_predicted, unioned_true = union_sparse_indices(
+        #     predicted_segmentation, sorted_true
+        # )
+        # assert torch.equal(unioned_predicted.indices(), unioned_true.indices())
 
         losses = []
         num = 2 * predicted_segmentation * sorted_true
         den = predicted_segmentation + sorted_true
         for num_i, den_i in zip(num, den):
-            num_sum = num_i.sum()
-            den_sum = den_i.sum()
+            num_sum = num_i.coalesce().values().sum()
+            den_sum = den_i.coalesce().values().sum()
             losses.append(1 - (num_sum + 1) / (den_sum + 1))
         loss = torch.stack(losses).mean()
         return loss

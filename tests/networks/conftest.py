3814b79e4e3c989979ba35db4d4b7606fc6aad87
Matthew Wright <mawright@lbl.gov>
2025-05-05 15:59:31 -0700

    fix int bounds

--- tests/networks/conftest.py
+++ tests/networks/conftest.py
@@ -127,7 +127,7 @@ def positions_strategy(draw):
         )
         max_position = draw(st.floats(min_value=min_position, max_value=1e30))
     else:
-        min_position = draw(st.integers(min_value=int(-1e10), max_value=int(1e10)))
+        min_position = draw(st.integers(min_value=int(-1e10), max_value=int(1e10) - 1))
         max_position = draw(st.integers(min_value=min_position, max_value=int(1e10)))
     return positions_dtype, min_position, max_position
 

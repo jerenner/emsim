d31ff6258e8073acb5765eec17e631dc988294f8
Matthew Wright <mawright@lbl.gov>
2025-05-14 12:42:07 -0700

    update sparse tensor scatter

--- emsim/utils/sparse_utils/indexing/scatter.py
+++ emsim/utils/sparse_utils/indexing/scatter.py
@@ -1,4 +1,4 @@
-from emsim.utils.sparse_utils.indexing.script_funcs import linearize_sparse_and_index_tensors
+from emsim.utils.sparse_utils.indexing.script_funcs import get_sparse_index_mapping
 
 
 import torch
@@ -6,7 +6,10 @@ from torch import Tensor
 
 
 def scatter_to_sparse_tensor(
-    sparse_tensor: Tensor, index_tensor: Tensor, values: Tensor
+    sparse_tensor: Tensor,
+    index_tensor: Tensor,
+    values: Tensor,
+    check_all_specified: bool = False,
 ) -> Tensor:
     """Batch updating of elements in a torch sparse tensor. Should be
     equivalent to sparse_tensor[index_tensor] = values. It works by flattening
@@ -16,11 +19,11 @@ def scatter_to_sparse_tensor(
 
     Args:
         sparse_tensor (Tensor): Sparse tensor of dimension ..., M; where ... are
-        S leading sparse dimensions and M is the dense dimension
+            S leading sparse dimensions and M is the dense dimension
         index_tensor (Tensor): Long tensor of dimension ..., S; where ... are
-        leading batch dimensions.
+            leading batch dimensions.
         values (Tensor): Tensor of dimension ..., M; where ... are leading
-        batch dimensions and M is the dense dimension
+            batch dimensions and M is the dense dimension
 
     Returns:
         Tensor: sparse_tensor with the new values scattered into it
@@ -34,26 +37,38 @@ def scatter_to_sparse_tensor(
     assert sparse_tensor.dense_dim() == values.ndim - 1
 
     sparse_tensor_values = sparse_tensor.values()
-    (
-        sparse_tensor_indices_linearized,
-        index_tensor_linearized,
-    ) = linearize_sparse_and_index_tensors(sparse_tensor, index_tensor)
-
-    index_search = torch.searchsorted(
-        sparse_tensor_indices_linearized,
-        index_tensor_linearized,
-    ).clamp_max(sparse_tensor_indices_linearized.shape[0] - 1)
-
-    is_specified_mask = (
-        sparse_tensor_indices_linearized[index_search] == index_tensor_linearized
+    index_search, is_specified_mask = get_sparse_index_mapping(
+        sparse_tensor, index_tensor
     )
 
-    new_values = sparse_tensor_values.clone()
-    new_values[index_search[is_specified_mask]] = values[is_specified_mask]
-    new_values = torch.cat([new_values, values[~is_specified_mask]], 0)
-    new_indices = torch.cat(
-        [sparse_tensor.indices(), index_tensor[~is_specified_mask].T], -1
-    )
+    all_specified = torch.all(is_specified_mask)
+
+    if check_all_specified and not all_specified:
+        raise ValueError(
+            "`check_all_specified` was set to True but not all gathered values "
+            "were specified"
+        )
+
+    if all_specified:
+        new_values = sparse_tensor_values.index_copy(
+            0, index_search[is_specified_mask], values[is_specified_mask]
+        )
+        new_indices = sparse_tensor.indices()
+    else:
+        n_old = sparse_tensor_values.size(0)
+        n_old_plus_new = n_old + (~is_specified_mask).sum()
+        new_values: Tensor = sparse_tensor_values.new_empty(
+            (n_old_plus_new,) + sparse_tensor_values.shape[1:]
+        )
+        new_values[:n_old] = sparse_tensor_values
+        new_values.index_copy_(
+            0, index_search[is_specified_mask], values[is_specified_mask]
+        )
+        new_values[n_old:] = values[~is_specified_mask]
+
+        new_indices = torch.cat(
+            [sparse_tensor.indices(), index_tensor[~is_specified_mask].T], -1
+        )
 
     out = torch.sparse_coo_tensor(
         new_indices,
@@ -61,5 +76,6 @@ def scatter_to_sparse_tensor(
         sparse_tensor.shape,
         dtype=sparse_tensor.dtype,
         device=sparse_tensor.device,
+        is_coalesced=all_specified.item(),
     ).coalesce()
     return out
